9efc60986a00e4e632495be5e5664dba479b3f25
iory <ab.ioryz@gmail.com>
2020-11-18 19:13:52 +0900

    Modified docker user namespace

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ docker run --rm \
        --env="DISPLAY" \
        --env="QT_X11_NO_MITSHM=1" \
        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-       -it k4a:melodic /bin/bash -i -c \
+       -it iory/k4a:melodic /bin/bash -i -c \
        "source /catkin_ws/devel/setup.bash && export ROS_MASTER_URI=http://localhost:11311 && roslaunch /kinectdriver_nodelet.launch camera:=${1:k4a} body_tracking_enabled:=false depth_mode:=WFOV_2X2BINNED color_resolution:=2160P fps:=30 point_cloud:=false rgb_point_cloud:=false publish_tf:=false color_format:=jpeg"
 xhost +local:docker
