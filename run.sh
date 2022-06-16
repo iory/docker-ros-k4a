b1f88dc56e9dbf463710e6c94e8389c4e914d820
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-06-16 16:53:00 +0900

    pass ros env in docker

--- run.sh
+++ run.sh
@@ -9,7 +9,9 @@ docker run --rm \
        --net=host \
        --env="DISPLAY" \
        --env="QT_X11_NO_MITSHM=1" \
+       --env="ROS_IP" \
+       --env="ROS_MASTER_URI" \
        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
        -it iory/docker-ros-k4a:latest /bin/bash -i -c \
-       "source /catkin_ws/devel/setup.bash && export ROS_MASTER_URI=http://localhost:11311 && roslaunch /kinectdriver_nodelet.launch camera:=${1:k4a} body_tracking_enabled:=true depth_mode:=WFOV_2X2BINNED color_resolution:=720P fps:=30 point_cloud:=false rgb_point_cloud:=false publish_tf:=false color_format:=jpeg"
+       "source /catkin_ws/devel/setup.bash && roslaunch /kinectdriver_nodelet.launch camera:=${1:k4a} body_tracking_enabled:=true depth_mode:=WFOV_2X2BINNED color_resolution:=720P fps:=30 point_cloud:=false rgb_point_cloud:=false publish_tf:=false color_format:=jpeg"
 xhost +local:docker
