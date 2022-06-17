b78c9911b8215a641fde5d93acdde22db8c72cbe
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-06-18 00:30:17 +0900

    refactor run.bash

--- run.sh
+++ run.sh
@@ -17,5 +17,5 @@ docker run --rm \
        --env="ROS_MASTER_URI" \
        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
        -it iory/docker-ros-k4a:latest /bin/bash -i -c \
-       "source /catkin_ws/devel/setup.bash && roslaunch /kinectdriver_nodelet.launch camera:=${1:k4a} body_tracking_enabled:=true depth_mode:=WFOV_2X2BINNED color_resolution:=1440P fps:=30 color_format:=jpeg point_cloud:=false rgb_point_cloud:=false"
+       "source /catkin_ws/devel/setup.bash && roslaunch /kinectdriver_nodelet.launch camera:=${1:k4a} body_tracking_enabled:=true depth_mode:=WFOV_2X2BINNED color_resolution:=1440P fps:=30 point_cloud:=false rgb_point_cloud:=false color_format:=jpeg"
 xhost +local:docker
